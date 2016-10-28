6f5a6e087167c53275834cf552cb430144bc8db4
James Shi <jshi@student.42.us.org>
2016-10-27 18:28:53 -0700

    g

--- print_float_g.c
+++ print_float_g.c
@@ -6,17 +6,91 @@
 /*   By: jshi <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 01:50:49 by jshi              #+#    #+#             */
-/*   Updated: 2016/10/27 02:04:26 by jshi             ###   ########.fr       */
+/*   Updated: 2016/10/27 18:28:29 by jshi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "ft_printf.h"
 
-int		print_float_g(va_list *args, t_flags *f)
+static int	apply_flags(char *str, t_flags *f)
+{
+	int		ret;
+
+	if (f->zero == 1 && f->minus == 0)
+	{
+		if (f->sign == 1 && f->plus == 0 && f->space == 0)
+			prepend_char(&inte, '0', f->minwid);
+		else
+			prepend_char(&inte, '0', f->minwid - 1);
+	}
+	if (f->sign == -1)
+		prepend_str(&inte, "-");
+	else if (f->plus == 1)
+		prepend_str(&inte, "+");
+	else if (f->space == 1)
+		prepend_str(&inte, " ");
+	if (f->minus == 1)
+		append_char(&inte, ' ', f->minwid);
+	else
+		prepend_char(&inte, ' ', f->minwid);
+	ret = ft_strlen(inte);
+	ft_putstr(inte);
+	free(inte);
+	return (ret);
+}
+
+static int	apply_flags_e(char *str, int i, int j, t_flags *f)
+{
+	char	*inte;
+	char	*frac;
+	char	*exp;
+
+	j = (i - f->prec < j) ? j : i - f->prec + 1;
+	inte = ft_strsub(str, i, 1);
+	frac = ft_strsub(str, j, i - j);
+	ft_strrev(inte);
+	ft_strrev(frac);
+	if (f->pound == 1 || ft_strlen(frac) > 0)
+		append_str(&inte, ".");
+	append_str(&inte, frac);
+	if (f->pound == 1)
+		append_char(&inte, '0', f->prec + 1);
+	free(frac);
+	(f->conv == 'G') ? append_str(&inte, "E") : append_str(&inte, "e");
+	(i >= 6000) ? append_str(&inte, "+") : append_str(&inte, "-");
+	exp = ft_itoa_base((i >= 6000) ? (i - 6000) : (6000 - i));
+	append_str(&inte, exp);
+	free(exp);
+	return (apply_flags(inte, f));
+}
+
+static int	apply_flags_f(char *str, int i, int j, t_flags *f)
+{
+	char	*inte;
+	char	*frac;
+
+	j = (i - f->prec < j) ? j : i - f->prec + 1;
+	inte = ft_strsub(str, 6000, (i - 5999) <= 0 ? 1 : (i - 5999));
+	frac = ft_strsub(str, j, 6000 - j);
+	ft_strrev(inte);
+	ft_strrev(frac);
+	if (f->pound == 1 || ft_strlen(frac) > 0)
+		append_str(&inte, ".");
+	append_str(&inte, frac);
+	if (f->pound == 1)
+		append_char(&inte, '0', f->prec + 1);
+	free(frac);
+	free(str);
+	return (apply_flags(inte, f));
+}
+
+int			print_float_g(va_list *args, t_flags *f)
 {
 	long double	a;
+	char		*str;
 	int			i;
+	int			j;
 
 	if (f->lenmod == 7)
 		a = va_arg(*args, long double);
@@ -26,5 +100,15 @@ int		print_float_g(va_list *args, t_flags *f)
 	if ((i = handle_nan(a, f, 0)) >= 0)
 		return (i);
 	f->prec = (f->prec < 0) ? 6 : f->prec + !f->prec;
-	return (0);
+	str = get_float(a);
+	i = 11999;
+	while (str[i] == '0' && i >= 0)
+		i--;
+	i = i == -1 ? 6000 : i;
+	j = 0;
+	while (str[j] == '0' && j < i)
+		j++;
+	if (i - 6000 < -4 || i - 6000 >= f->prec)
+		return (apply_flags_e(str, i, j, f));
+	return (apply_flags_f(str, i, j, f));
 }
